0c8dacaf0d337af7f4b4948f14762e4cec87645a
firstfinancialcanada-art <firstfinancialcanada@gmail.com>
2026-02-04 11:18:53 -0700

    Update indexV2.0.js

--- indexV2.0.js
+++ indexV2.0.js
@@ -1,10 +1,1 @@
-Sukhveer, Singh Kaler
-587 371-9511
 
-COUPON
-• Dealership Name: Automaxx On 16th
-• Template Name: $500 Off
-• URL: automaxxon16th.com/inventory/Used-2015-Mercedes--Benz-C--Class-C_400-55SWF6GB7FU017258
-• Car Info: 2015 Mercedes-Benz C-Class C 400
-• Stock Number: AM24137A
-• VIN: 55SWF6GB7FU017258
